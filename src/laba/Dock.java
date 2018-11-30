fb3b650c978d184d8b3038be2246399299e4217e
Kurai <akokuro@mail.ru>
2018-11-30 14:56:32 +0400

    laba4

--- src/laba/Dock.java
+++ src/laba/Dock.java
@@ -4,9 +4,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
 
 public class Dock<T extends IShip> {
-	private T[] _places;
+	private HashMap<Integer, T> _places;
 
 	// Размер одного места парковки по ширине.
 	private int _placeSizeWidth = 200;
@@ -19,6 +21,8 @@ public class Dock<T extends IShip> {
 
 	// Размер парковки по высоте.
 	private int PictureHeight;
+	// Максимальное количество мест
+	private int _maxCount;
 
 	// Геттеры и сеттеры для PictureWudth и PictureHeight.
 	public int getPictureWidth() {
@@ -38,58 +42,52 @@ public class Dock<T extends IShip> {
 	}
 
 	// Конструктор дока.
-	public Dock(T[] places, int pictureWidth, int pictureHeight) {
-		_places = places;
+	public Dock(int size, int pictureWidth, int pictureHeight) {
+		_maxCount = size;
+		_places = new HashMap<Integer, T>(size);
 		PictureWidth = pictureWidth;
 		PictureHeight = pictureHeight;
-		for (int i = 0; i < _places.length; i++) {
-			_places[i] = null;
-		}
 	}
 
 	// Оператор "+": поставить корабль
 	public int Add(T ship) {
-		for (int i = 0; i < _places.length; i++) {
+		if (_places.size() == _maxCount) {
+			return -1;
+		}
+
+		for (int i = 0; i < _maxCount; i++) {
 			if (CheckFreePlace(i)) {
-				_places[i] = ship;
-				_places[i].SetPosition(5 + i / 5 * _placeSizeWidth + 5, i % 5 * _placeSizeHeight + _placeSizeHeight - 4, PictureWidth,
-						PictureHeight);
+				_places.put(i, ship);
+				_places.get(i).SetPosition(5 + i / 5 * _placeSizeWidth + 5,
+						i % 5 * _placeSizeHeight + _placeSizeHeight - 4, PictureWidth, PictureHeight);
 
 				return i;
 			}
 		}
-
 		return -1;
 	}
 
 	// Оператор "-": забрать корабль
 	public T Del(int index) {
-		if (index < 0 || index >= _places.length) {
-			return null;
-		}
-
-		if (!CheckFreePlace(index)) {
-			T ship = _places[index];
-			_places[index] = null;
-			return ship;
-		}
-
-		return null;
-	}
+		if (!CheckFreePlace(index))
+        {
+            T ship = _places.get(index);
+            _places.remove(index);
+            return ship;
+        }
+        return null;	}
 
 	// Проверить по номеру места, свободно ли оно
 	private boolean CheckFreePlace(int index) {
-		return _places[index] == null;
+		 return !_places.containsKey(index);
 	}
 
 	// Отрисовка дока
 	public void Draw(Graphics g) {
 		DrawMarking(g);
-		for (int i = 0; i < _places.length; i++) {
-			if (!CheckFreePlace(i)) {
-				_places[i].DrawShip(g);
-			}
-		}
+		 for (T ship: _places.values()) {
+	            ship.DrawShip(g);
+	        }
 	}
 
 	// Отрисовка линий
